d0f253e24a9d7e23009eb47878d858d2dc7c2d1b
danieltmc <me@danieltmc.com>
2019-08-19 22:27:12 -0400

    Added digital inputs for Tuba valves

--- arduino/valves/valves.ino
+++ arduino/valves/valves.ino
@@ -1,9 +1,25 @@
+const int valve1 = 16;
+const int valve2 = 5;
+const int valve3 = 4;
+const int valve4 = 0;
+int valve1_status = 0;
+int valve2_status = 0;
+int valve3_status = 0;
+int valve4_status = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(valve1, INPUT);
+  pinMode(valve2, INPUT);
+  pinMode(valve3, INPUT);
+  pinMode(valve4, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  valve1_status = digitalRead(valve1);
+  valve2_status = digitalRead(valve2);
+  valve3_status = digitalRead(valve3);
+  valve4_status = digitalRead(valve4);
 }
