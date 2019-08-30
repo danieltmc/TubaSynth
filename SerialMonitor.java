fb27325f7b2cc686bb4fc121a2b9cd3dc8cb50b9
danieltmc <me@danieltmc.com>
2019-08-30 10:42:29 -0400

    Monitor the given serial port using a buffer

--- SerialMonitor.java
+++ SerialMonitor.java
@@ -1,10 +1,25 @@
 import com.fazecast.jSerialComm.*;
 
 public class SerialMonitor
-{
-	public void connect(String port)
+{	
+	public void monitor_buffer()
 	{
-		
+		port.openPort();
+		try
+		{
+			while (port.bytesAvailable() == 0)
+			{
+				Thread.sleep(10);
+			}
+			byte[] buffer = new byte[port.bytesAvailable()];
+			port.readBytes(buffer, buffer.length);
+			// TODO: Decode bytes and play tone
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		port.closePort();
 	}
 	
 	public static SerialPort[] get_ports()
