36f15530e9af4ab4eadd972351ecdb76058bf4bf
danieltmc <me@danieltmc.com>
2019-08-29 23:33:30 -0400

    Identify available ports

--- SerialMonitor.java
+++ SerialMonitor.java
@@ -1,6 +1,24 @@
-import java.comm.*;
+import javax.comm.*;
 
 public class SerialMonitor
 {
+	public void connect(String port)
+	{
+		
+	}
 	
+	public void list_ports()
+	{
+		java.util.Enumeration<CommPortIdentifier> port_enum = CommPortIdentifier.getPortIdentifiers();
+		while (port_enum.hasMoreElements())
+		{
+			CommPortIdentifier identifier = port_enum.nextElement();
+			System.out.println(identifier.getName() + " - " );//+ );
+		}
+	}
+	
+	public String port_type(int type)
+	{
+		return(null);
+	}
 }
