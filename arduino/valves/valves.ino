6fa50f06c64df14f902f4288edf32914b9127ea3
danieltmc <me@danieltmc.com>
2019-08-30 20:42:41 -0400

    Changed method of encoding serial values for valve and wind input

--- arduino/valves/valves.ino
+++ arduino/valves/valves.ino
@@ -2,13 +2,18 @@ const int valve1 = 16;
 const int valve2 = 5;
 const int valve3 = 4;
 const int valve4 = 0;
+const int wind = A0;
 const int interrupt = 2;
 
-int valve1_status = 0;
-int valve2_status = 0;
-int valve3_status = 0;
-int valve4_status = 0;
-int interrupt_status = 0;
+int valve1_status = 0; // 0-1
+int valve2_status = 0; // 0-1
+int valve3_status = 0; // 0-1
+int valve4_status = 0; // 0-1
+int wind_status = 0; // 0-1023
+int interrupt_status = 0; // 0-1
+
+int valve_combo = 0;
+int encoded = 0;
 
 void setup()
 {
@@ -17,6 +22,7 @@ void setup()
   pinMode(valve2, INPUT);
   pinMode(valve3, INPUT);
   pinMode(valve4, INPUT);
+  pinMode(wind, INPUT);
   pinMode(interrupt, INPUT);
 }
 
@@ -26,96 +32,104 @@ void loop()
   valve2_status = digitalRead(valve2);
   valve3_status = digitalRead(valve3);
   valve4_status = digitalRead(valve4);
+  wind_status = analogRead(wind);
   interrupt_status = digitalRead(interrupt);
-
+  
+  // Stop program
   if (interrupt_status)
   {
     Serial.write(-1);
     return;
   }
-  // 0000
-  if (!valve1_status & !valve2_status & !valve3_status & !valve4_status)
+  // No sound
+  else if (!wind_status)
   {
     Serial.write(0);
+    return;
   }
-  // 1000
+  // 0000 - Open valves
+  else if (!valve1_status & !valve2_status & !valve3_status & !valve4_status)
+  {
+    valve_combo = 0;
+  }
+  // 1000 - First valve
   else if (valve1_status & !valve2_status & !valve3_status & !valve4_status)
   {
-    Serial.write(1);
+    valve_combo = 1;
   }
-  // 0100
+  // 0100 - Second valve
   else if (!valve1_status & valve2_status & !valve3_status & !valve4_status)
   {
-    Serial.write(2);
+    valve_combo = 2;
   }
-  // 0010
+  // 0010 - Third valve
   else if (!valve1_status & !valve2_status & valve3_status & !valve4_status)
   {
-    Serial.write(3);
+    valve_combo = 3;
   }
-  // 0001
+  // 0001 - Fourth valve
   else if (!valve1_status & !valve2_status & !valve3_status & valve4_status)
   {
-    Serial.write(4);
+    valve_combo = 4;
   }
-  // 1100
+  // 1100 - First and Second
   else if (valve1_status & valve2_status & !valve3_status & !valve4_status)
   {
-    Serial.write(12);
+    valve_combo = 12;
   }
-  // 1010
+  // 1010 - First and Third
   else if (valve1_status & !valve2_status & valve3_status & !valve4_status)
   {
-    Serial.write(13);
+    valve_combo = 13;
   }
-  // 1001
+  // 1001 - First and Fourth
   else if (valve1_status & !valve2_status & !valve3_status & valve4_status)
   {
-    Serial.write(14);
+    valve_combo = 14;
   }
-  // 0110
+  // 0110 - Second and Third
   else if (!valve1_status & valve2_status & valve3_status & !valve4_status)
   {
-    Serial.write(23);
+    valve_combo = 23;
   }
-  // 0101
+  // 0101 - Second and Fourth
   else if (!valve1_status & valve2_status & !valve3_status & valve4_status)
   {
-    Serial.write(24);
+    valve_combo = 24;
   }
-  // 0011
+  // 0011 - Third and Fourth
   else if (!valve1_status & !valve2_status & valve3_status & valve4_status)
   {
-    Serial.write(34);
+    valve_combo = 34;
   }
-  // 1110
+  // 1110 - First, Second, and Third
   else if (valve1_status & valve2_status & valve3_status & !valve4_status)
   {
-    Serial.write(123);
+    valve_combo = 123;
   }
-  // 1101
+  // 1101 - First, Second, and Fourth
   else if (valve1_status & valve2_status & !valve3_status & valve4_status)
   {
-    Serial.write(124);
+    valve_combo = 124;
   }
-  // 1011
+  // 1011 - First, Third, and Fourth
   else if (valve1_status & !valve2_status & valve3_status & valve4_status)
   {
-    Serial.write(134);
+    valve_combo = 134;
   }
-  // 0111
+  // 0111 - Second, Third, and Fourth
   else if (!valve1_status & valve2_status & valve3_status & valve4_status)
   {
-    Serial.write(234);
+    valve_combo = 234;
   }
-  // 1111
+  // 1111 - All valves
   else if (valve1_status & valve2_status & valve3_status & valve4_status)
   {
-    Serial.write(1234);
+    valve_combo = 1234;
   }
-  //Serial.print(valve1_status);
-  //Serial.print(valve2_status);
-  //Serial.print(valve3_status);
-  //Serial.print(valve4_status);
-  //Serial.print("\n");
+  
+  encoded = (valve_combo << 10) + wind_status;
+  Serial.write(encoded);
+  
+  //Serial.println(valve_combo);
 }
